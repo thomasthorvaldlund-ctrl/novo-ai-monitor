463ef25f7451be8e3d40348a865a6b98499e5246
Thomas Lund <thomas.thorvald.lund@gmail.com>
2026-07-04 09:15:25 +0000

    Start moving Portfolio Analysis logic

--- portfolio_analysis_routes.py
+++ portfolio_analysis_routes.py
@@ -3,11 +3,12 @@ import yfinance as yf
 
 portfolio_analysis_bp = Blueprint("portfolio_analysis", __name__)
 
-from flask import Blueprint
-
-portfolio_analysis_bp = Blueprint("portfolio_analysis", __name__)
-
 
 @portfolio_analysis_bp.route("/portfolio-analysis-page")
 def portfolio_analysis_page():
-    return "Portfolio Analysis blueprint works"
+    novo_qty = 23
+    novo_buy_price = 301.3
+    dsv_qty = 4
+    dsv_buy_price = 1588.5
+
+    return "OK"
