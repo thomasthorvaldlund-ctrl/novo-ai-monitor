1d269284349643c7bc2844986907626f88bca6ac
Thomas Lund <thomas.thorvald.lund@gmail.com>
2026-07-04 11:56:46 +0000

    Add Market Dashboard blueprint test route

--- market_dashboard_routes.py
+++ market_dashboard_routes.py
@@ -1,3 +1,7 @@
 from flask import Blueprint
 
 market_dashboard_bp = Blueprint("market_dashboard", __name__)
+
+@market_dashboard_bp.route("/market-dashboard")
+def market_dashboard():
+    return "Market Dashboard blueprint works"
