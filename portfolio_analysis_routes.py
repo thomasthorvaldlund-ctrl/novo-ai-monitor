27090d42380dfd965c24e4bd657ae9bfa1057733
Thomas Lund <thomas.thorvald.lund@gmail.com>
2026-07-04 09:11:22 +0000

    Add Portfolio Analysis blueprint test route

--- portfolio_analysis_routes.py
+++ portfolio_analysis_routes.py
@@ -1,3 +1,8 @@
 from flask import Blueprint
 
 portfolio_analysis_bp = Blueprint("portfolio_analysis", __name__)
+
+
+@portfolio_analysis_bp.route("/portfolio-analysis-page")
+def portfolio_analysis_page():
+    return "Portfolio Analysis blueprint works"
