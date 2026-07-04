ee27056f5cf8bbb541c4459e1c3cf06db62559be
Thomas Lund <thomas.thorvald.lund@gmail.com>
2026-07-04 11:15:12 +0000

    Rename Portfolio Analysis data function

--- portfolio_analysis_routes.py
+++ portfolio_analysis_routes.py
@@ -5,7 +5,7 @@ portfolio_analysis_bp = Blueprint("portfolio_analysis", __name__)
 
 
 @portfolio_analysis_bp.route("/portfolio-analysis-page")
-def portfolio_analysis_page():
+def portfolio_analysis():
     novo_qty = 23
     novo_buy_price = 301.3
     dsv_qty = 4
