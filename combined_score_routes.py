cb274174adb30790e71ec5a50bcc8f97bac57f68
Thomas Lund <thomas.thorvald.lund@gmail.com>
2026-07-05 09:17:10 +0000

    Add Combined Score blueprint test route

--- combined_score_routes.py
+++ combined_score_routes.py
@@ -1,3 +1,7 @@
 from flask import Blueprint
 
 combined_score_bp = Blueprint("combined_score", __name__)
+
+@combined_score_bp.route("/combined-stock-score")
+def combined_stock_score():
+    return "Combined Score blueprint works"
