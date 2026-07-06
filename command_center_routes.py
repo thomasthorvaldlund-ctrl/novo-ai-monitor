648e13f7804f8265dc466846b49f33f6b92d4e62
Thomas Lund <thomas.thorvald.lund@gmail.com>
2026-07-06 08:28:44 +0000

    Add Market Summary to Command Center

--- command_center_routes.py
+++ command_center_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 
+
 from market_score_service import get_market_score
+from market_summary_service import get_market_summary
 from top_picks_service import get_top_picks
 from combined_score_service import combined_stock_score as service_combined_score
 from openai_service import client
@@ -10,6 +12,7 @@ command_center_bp = Blueprint("command_center", __name__)
 @command_center_bp.route("/command-center")
 def command_center():
     market = get_market_score()
+    summary = get_market_summary()
 
     combined_data = service_combined_score(client)
     ranking = combined_data.get("combined_ranking", [])
@@ -20,4 +23,5 @@ def command_center():
         "command_center.html",
         market=market,
         top_picks=top_picks,
+        summary=summary,
     )
