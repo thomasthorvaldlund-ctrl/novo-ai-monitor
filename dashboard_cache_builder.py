dfce5dfd89d7871feee5ed89acc11a08443793da
Thomas Lund <thomas.thorvald.lund@gmail.com>
2026-07-12 11:40:03 +0000

    Add AI news summary to dashboard cache

--- dashboard_cache_builder.py
+++ dashboard_cache_builder.py
@@ -13,6 +13,7 @@ from system_health_service import get_system_health
 from morning_brief_service import get_morning_brief
 from performance_service import get_signal_statistics
 from dashboard_cache_service import save_dashboard_cache
+from news_sentiment_service import get_ai_news_sentiment
 
 def build_dashboard_cache():
 
@@ -30,6 +31,7 @@ def build_dashboard_cache():
     "analyst": get_ai_analyst(),
     "morning_brief": get_morning_brief(),
     "performance": get_signal_statistics(),
+    "ai_news": get_ai_news_sentiment(),
 }
 
     save_dashboard_cache(data)
